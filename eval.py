319e4812d34ad3207b7b530e800cb49e1d745e41
nik1806 <nikhil.paliwal14@gmail.com>
2023-06-15 15:33:01 +0200

    update eval script for vis and test with synthia_seq

--- eval.py
+++ eval.py
@@ -13,6 +13,8 @@ from easydict import EasyDict as edict
 from tqdm import tqdm
 from PIL import Image
 from utils.mwc import mwc_refiner
+from datetime import datetime
+from matplotlib import pyplot as plt
 
 palette = [128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156, 190, 153, 153, 153, 153, 153, 250, 170, 30,
            220, 220, 0, 107, 142, 35, 152, 251, 152, 70, 130, 180, 220, 20, 60, 255, 0, 0, 0, 0, 142, 0, 0, 70,
@@ -36,13 +38,13 @@ def get_arguments():
     """
     parser = argparse.ArgumentParser(description="DeepLab-ResNet Network")
     parser.add_argument("--ignore-label", type=int, default=255)
-    parser.add_argument("--num-classes", type=int, default=19)
+    parser.add_argument("-n", "--num-classes", type=int, default=12)
     parser.add_argument("--frm", type=str, default=None)
     parser.add_argument("--start", type=int, default=1)
-    parser.add_argument("--dataset", type=str, default='cityscapes')
+    parser.add_argument("--dataset", type=str, default='synthia_seq') #'cityscapes'
     parser.add_argument("--single", action='store_true')
     parser.add_argument("--model", default='deeplab')
-    parser.add_argument("--source", default='gta5')
+    parser.add_argument("--source", default='synthia_seq')
     return parser.parse_args()
 
 def print_iou(iou, acc, miou, macc):
@@ -53,7 +55,7 @@ def print_iou(iou, acc, miou, macc):
 def compute_iou(model, testloader, args):
     model = model.eval()
 
-    interp = nn.Upsample(size=(1024, 2048), mode='bilinear', align_corners=True)
+    interp = nn.Upsample(size=(730, 1460), mode='bilinear', align_corners=True) #(1024, 2048)
     union = torch.zeros(args.num_classes, 1,dtype=torch.float).cuda().float()
     inter = torch.zeros(args.num_classes, 1, dtype=torch.float).cuda().float()
     preds = torch.zeros(args.num_classes, 1, dtype=torch.float).cuda().float()
@@ -67,14 +69,14 @@ def compute_iou(model, testloader, args):
             src_output = interp(output)
 
             ##!! MWC solver
-            output, mb = mwc_refiner(src_output, name)
-            pred = output.float().cuda()
-            _, H, W = output.shape; C=19
+            # output, mb = mwc_refiner(src_output, name)
+            # pred = output.float().cuda()
+            # _, H, W = output.shape; C=19
 
-            ##!! original method
-            # output = src_output.squeeze()
-            # pred = output.argmax(dim=0).float().cuda()
-            # C, H, W = output.shape 
+            #!! original method
+            output = src_output.squeeze()
+            pred = output.argmax(dim=0).float().cuda()
+            C, H, W = output.shape 
 
             Mask = (label.squeeze())<C
 
@@ -83,7 +85,7 @@ def compute_iou(model, testloader, args):
             pred_mask = torch.eq(pred_e, pred).byte()
             pred_mask = pred_mask*Mask
             
-            # save_results(image, label, src_output.squeeze(0).argmax(dim=0), pred, mb) ##!!
+            # save_results(image, label, src_output.squeeze(0).argmax(dim=0), None, None, args.num_classes) #pred,mb) ##!!
 
             label_e = torch.linspace(0,C-1, steps=C).view(C, 1, 1)
             label_e = label_e.repeat(1, H, W).cuda()
@@ -102,7 +104,7 @@ def compute_iou(model, testloader, args):
 
         iou = inter/union
         acc = inter/preds
-
+        # print(args.source)
         if args.source=='synthia':
             iou = iou.squeeze()
             class16_iou = torch.cat((iou[:9], iou[10:14], iou[15:16], iou[17:]))
@@ -110,39 +112,64 @@ def compute_iou(model, testloader, args):
             class13_iou = torch.cat((class16_iou[:3], class16_iou[6:]))
             class13_miou = class13_iou.mean().item()
             print('16-Class mIoU:{:.2%}'.format(class16_miou))
-            print(class13_iou)
-            print('13-Class mIoU:{:.2%}'.format(class13_miou))
             print(class16_iou)
-
-        mIoU = iou.mean().item()
-        mAcc = acc.mean().item()
+            print('13-Class mIoU:{:.2%}'.format(class13_miou))
+            print(class13_iou)
+        elif args.source=='synthia_seq':
+            iou = iou.squeeze()
+            iou = torch.cat((iou[:3], iou[4:])).unsqueeze(dim=1)
+            mIoU = iou.mean().item()
+            print('11-Class mIoU:{:.2%}'.format(mIoU))
+            # acc = [None for _ in range(len(iou))]
+            # mAcc = None
+            acc = acc.squeeze()
+            acc = torch.cat((acc[:3], acc[4:])).unsqueeze(dim=1)
+            mAcc = acc.mean().item()
+            # print(class11_iou)
+        else:
+            mIoU = iou.mean().item()
+            mAcc = acc.mean().item()
+        
         print_iou(iou, acc, mIoU, mAcc)
         return iou, mIoU, acc, mAcc
     
-def decode_segmap(temp, kwargs=None):
+def decode_segmap(temp, kwargs=None, cls=19):
     colors = [  # [  0,   0,   0],
-        [128, 64, 128],
-        [244, 35, 232],
-        [70, 70, 70],
-        [102, 102, 156],
-        [190, 153, 153],
-        [153, 153, 153],
-        [250, 170, 30],
-        [220, 220, 0],
-        [107, 142, 35],
-        [152, 251, 152],
-        [0, 130, 180],
-        [220, 20, 60],
-        [255, 0, 0],
-        [0, 0, 142],
-        [0, 0, 70],
-        [0, 60, 100],
-        [0, 80, 100],
-        [0, 0, 230],
-        [119, 11, 32],
+        # [128, 64, 128],
+        # [244, 35, 232],
+        # [70, 70, 70],
+        # [102, 102, 156],
+        # [190, 153, 153],
+        # [153, 153, 153],
+        # [250, 170, 30],
+        # [220, 220, 0],
+        # [107, 142, 35],
+        # [152, 251, 152],
+        # [0, 130, 180],
+        # [220, 20, 60],
+        # [255, 0, 0],
+        # [0, 0, 142],
+        # [0, 0, 70],
+        # [0, 60, 100],
+        # [0, 80, 100],
+        # [0, 0, 230],
+        # [119, 11, 32],
+        [128,64,128],
+        [244,35,232],
+        [70,70,70],
+        [190,153,153],
+        [153,153,153],
+        [250,170,30],
+        [220,220,0],
+        [107,142,35],
+        [70,130,180],
+        [220,20,60],
+        [255,0,0],
+        [0,0,142],
     ]
 
-    label_colours = dict(zip(range(19), colors))
+    label_colours = dict(zip(range(cls), colors))
+
     '''
     Convert interger class labels to color image for representing segmentations.
     '''
@@ -150,7 +177,7 @@ def decode_segmap(temp, kwargs=None):
     r = temp.copy()
     g = temp.copy()
     b = temp.copy()
-    for l in range(0, 19):
+    for l in range(0, cls):
         r[temp == l] = label_colours[l][0]
         g[temp == l] = label_colours[l][1]
         b[temp == l] = label_colours[l][2]
@@ -161,10 +188,11 @@ def decode_segmap(temp, kwargs=None):
     rgb[:, :, 2] = b / 255.0
     return rgb    
 
-def save_results(image, gt, source, out, mb):
-    from matplotlib import pyplot as plt
+def save_results(image, gt, source, out, mb, cls):
+    now = datetime.now()
+
     fig = plt.figure(figsize=(40,20))#figsize
-    col=5# if mb else 4
+    col=3# if mb else 4
     
     fig.add_subplot(1,col,1)
     image = image.squeeze(0).permute(1,2,0)
@@ -175,33 +203,34 @@ def save_results(image, gt, source, out, mb):
     plt.title("Original"); plt.axis('off')
 
     fig.add_subplot(1,col,2)
-    gt_segmap = decode_segmap(gt.squeeze(0).data.cpu().numpy())
+    gt_segmap = decode_segmap(gt.squeeze(0).data.cpu().numpy(), cls=cls)
     plt.imshow(gt_segmap)
     plt.title("Ground truth"); plt.axis('off')
 
     fig.add_subplot(1,col,3)
-    source_segmap = decode_segmap(source.data.cpu().numpy())
+    source_segmap = decode_segmap(source.data.cpu().numpy(), cls=cls)
     plt.imshow(source_segmap)
     plt.title("DASS"); plt.axis('off')
 
-    fig.add_subplot(1,col,4)
-    out_segmap = decode_segmap(out.squeeze(0).data.cpu().numpy())
-    plt.imshow(out_segmap)
-    plt.title("MWC"); plt.axis('off')
+    # fig.add_subplot(1,col,4)
+    # out_segmap = decode_segmap(out.squeeze(0).data.cpu().numpy())
+    # plt.imshow(out_segmap)
+    # plt.title("MWC"); plt.axis('off')
 
-    if True:
-        fig.add_subplot(1,col,5)
-        plt.imshow(mb)
-        plt.title("Motion boundary"); plt.axis('off')
+    # if True:
+    #     fig.add_subplot(1,col,5)
+    #     plt.imshow(mb)
+    #     plt.title("Motion boundary"); plt.axis('off')
 
-    plt.savefig("results/cityscapes_seg.png", bbox_inches='tight')
+    plt.savefig(f"results/synthia2city_{now.day}_{now.hour}_{now.minute}_{now.second}.png", bbox_inches='tight')
     plt.plot()
-    exit() ##!! change to show multiple random instances
+    # exit() ##!! change to show multiple random instances
 
 
 def main():
     args = get_arguments()
-    with open('./config/damnet_config_upsize.yml') as f:
+    # with open('./config/damnet_config_upsize.yml') as f:
+    with open('config/so_config.yml') as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
     cfg = edict(cfg)
     cfg.num_classes=args.num_classes
@@ -213,7 +242,8 @@ def main():
         #model_path = './results/dam/snapshot/train/GTA5_best.pth'
         #model_path = './results/synthia_source_only/snapshot/train/wonkyung.pth'
         # model_path = './results/dam2/snapshot/train/Synthia_best.pth'
-        model_path = '/share_chairilg/weights/gta5_deepv2_trained_dass51.pth'
+        # model_path = '/share_chairilg/weights/gta5_deepv2_trained_dass51.pth'
+        model_path = './results/synthia_source_only/snapshot/train/synthia_best_source_only.pth'
         model = ResPair_Deeplab(num_classes=args.num_classes)
         #model = nn.DataParallel(model)
         model.load_state_dict(torch.load(model_path))
